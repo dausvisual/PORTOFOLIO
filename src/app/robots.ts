c40e4eb19f1f363d3db1a47045bfc7993f7199c8
Firdaus Ikram <dausvisual@gmail.com>
2026-07-17 09:51:35 +0800

    update robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,18 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/private/',
-      },
-      {
-        // Secara eksplisit mengizinkan AI crawlers
-        userAgent: ['GPTBot', 'ChatGPT-User', 'Google-Extended', 'Googlebot', 'Googlebot-Image', 'Bingbot', 'Slurp', 'DuckDuckBot'],
-        allow: '/',
-      }
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
     sitemap: 'https://firdausikram.com/sitemap.xml',
   }
 }
